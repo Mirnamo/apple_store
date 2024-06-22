69e862e87fe924f7d36e2f23d41c7de0cc0b9a2a
Mirnamo <mirnamohamed594@gmail.com>
2024-06-21 23:06:10 -0400

    near done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
+import * as Sentry from '@sentry/react';
+import Features from './components/Features';
+import Footer from './components/Footer';
+import VideoAnimation from './components/VideoAnimation';
 
-const App = () => {
-
-  return (
+ const App = () => {
+  //test sentry error catch
+ //return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+   return (
     <main className='bg-black'>
        <Navbar />
        <Hero />
        <Highlights />
        <Model />
+       <Features/>
+       <Footer/>
+       <VideoAnimation/>
     </main>
   )
 }
 
-export default App;
+export default Sentry.withProfiler(App);
---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { footerLinks } from '../constants'
+
+const Footer = () => {
+  return (
+    <footer className="py-5 sm:px-10 px-5">
+      <div className="screen-max-width">
+        <div>
+          <p className="font-semibold text-gray text-xs">
+            More ways to shop: {' '}
+            <a href="https://www.apple.com/retail/" className="underline text-blue">Find an Apple Store</a>
+            {' '} or {' '}
+            <a href="https://www.apple.com/retail/storelist/" className="text-blue underline">other retailer</a>
+            {' '}
+            near you.
+          </p>
+          <p className="font-semibold text-gray text-xs">
+            Or call 000800-040-1966
+          </p>
+        </div>
+
+        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
+
+        <div className="flex md:flex-row flex-col md:items-center justify-between">
+          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
+          <div className="flex">
+            {footerLinks.map((link, i) => (
+              <p key={link} className="font-semibold text-gray text-xs">
+                {link}{' '}
+                {i !== footerLinks.length - 1 && (
+                  <span className="mx-2"> | </span>
+                )}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
