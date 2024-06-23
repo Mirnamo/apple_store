6966084c5a65e0f242b0696489aef36b6181701c
Mirnamo <mirnamohamed594@gmail.com>
2024-06-22 23:29:53 -0400

    last modify I gues..

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
-import * as Sentry from '@sentry/react';
 import Features from './components/Features';
 import Footer from './components/Footer';
 import VideoAnimation from './components/VideoAnimation';
 
  const App = () => {
-  //test sentry error catch
- //return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
    return (
     <main className='bg-black'>
        <Navbar />
@@ -23,4 +20,4 @@ import VideoAnimation from './components/VideoAnimation';
   )
 }
 
-export default Sentry.withProfiler(App);
+export default App;
